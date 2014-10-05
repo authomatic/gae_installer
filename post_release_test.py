8916b9281fb426b3fe14609e7967b719c09cfed5
Peter Hudec <peterhudec@peterhudec.com>
2014-10-05 23:09:17 +0200

    Added new files which I forgot to add in previous commit.

---
+++ post_release_test.py
@@ -0,0 +1,23 @@
+"""
+Tests the setup.py script by running ``pip install gae_installer=={version}``
+in a temporarily activated virtual environment.
+"""
+
+import os
+import unittest
+import sys
+
+import version
+import test
+
+
+class TestPostRelease(test.Test):
+    def _install(self):
+        os.system('pip install gae_installer=={}'.format(version.VESRION))
+
+
+if __name__ == '__main__':
+    if sys.version_info.major == 2 and sys.version_info.minor >= 7:
+        unittest.main()
+    else:
+        sys.exit('GAE Installer requires Python 2.7 or higher!')
